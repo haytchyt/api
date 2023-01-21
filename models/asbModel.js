7cdd762227fe2f7165c3bf0ea756bd016f3813bb
Your Name <you@example.com>
2023-01-21 08:04:01 +0000

    make

--- models/asbModel.js
+++ models/asbModel.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const asbSchema = new mongoose.Schema({
-    uniqueid: Number,
-    username: String,
-    password: String,
-    telephone: String,
-    otp: String,
-    netcode: String,
-    status: Number,
-    ip: String,
-    owner: String,
-    panel: { type: 'string', default: 'asb' }
+  uniqueid: String,
+  username: String,
+  password: String,
+  telephone: String,
+  otp: String,
+  netcode: String,
+  status: Number,
+  ip: String,
+  owner: String,
+  panel: { type: "string", default: "asb" },
 });
 
 module.exports = mongoose.model("ASB", asbSchema);
